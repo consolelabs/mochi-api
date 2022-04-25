d9410e7e1ad9e8a518ef482cf5d8f2e347e0faf5
Bien Vo <bienvh@dwarvesv.com>
2022-04-25 11:58:10 +0700

    feat: invite tracker - leaderboard and link

--- pkg/repo/invite_histories/store.go
+++ pkg/repo/invite_histories/store.go
@@ -1,8 +1,12 @@
 package invite_histories
 
-import "github.com/defipod/mochi/pkg/model"
+import (
+	"github.com/defipod/mochi/pkg/model"
+	"github.com/defipod/mochi/pkg/response"
+)
 
 type Store interface {
 	Create(invite *model.InviteHistory) error
 	CountByInviter(inviterID int64) (int64, error)
+	GetInvitesLeaderboard(guildID string) ([]response.LeaderboardRecord, error)
 }
