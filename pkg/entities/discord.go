1ab6b1e337b84d6f2346d4a610fb2ade1d95890e
trkhoi <ngotrongkhoi112@gmail.com>
2022-05-06 09:23:43 +0700

    fix: review

--- pkg/entities/discord.go
+++ pkg/entities/discord.go
@@ -2,9 +2,12 @@ package entities
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
+	"github.com/defipod/mochi/pkg/consts"
+	"github.com/defipod/mochi/pkg/logger"
 	"github.com/defipod/mochi/pkg/response"
 )
 
@@ -46,95 +49,107 @@ func (e *Entity) GetGuildUsersFromDiscord(guildID string) ([]response.DiscordGui
 }
 
 func (e *Entity) CountGuildChannels(guildID string) (int, int, int, int, int, int, error) {
+	log := logger.NewLogrusLogger()
 	nr_of_channels, nr_of_text_channels, nr_of_voice_channels, nr_of_stage_channels, nr_of_categories, nr_of_announcement_channels := 0, 0, 0, 0, 0, 0
 	guildChannels, err := e.discord.GuildChannels(guildID)
-
 	if err != nil {
+		log.Error(err, "failed to get guild channels")
 		return 0, 0, 0, 0, 0, 0, err
 	}
-
 	if len(guildChannels) == 0 {
+		log.Info("Members not exist in current guild")
 		return 0, 0, 0, 0, 0, 0, err
 	}
-
 	for _, channel := range guildChannels {
-		if channel.Type == 0 {
+		// https://discord.com/developers/docs/resources/channel#channel-object-channel-types
+		// Refer to discord doc: 0 -> text channel, 2 -> voice channel, 4 -> category, 5 -> announcement channel, 13 -> stage channel
+		switch channel.Type {
+		case consts.TextChannel:
 			nr_of_text_channels = nr_of_text_channels + 1
-		} else if channel.Type == 2 {
+		case consts.VoiceChannel:
 			nr_of_voice_channels = nr_of_voice_channels + 1
-		} else if channel.Type == 4 {
+		case consts.Category:
 			nr_of_categories = nr_of_categories + 1
-		} else if channel.Type == 5 {
+		case consts.AnnouncementChannel:
 			nr_of_announcement_channels = nr_of_announcement_channels + 1
-		} else if channel.Type == 13 {
+		case consts.StageChannel:
 			nr_of_stage_channels = nr_of_stage_channels + 1
+		default:
+			fmt.Println("still not handle this case")
 		}
 	}
-
 	nr_of_channels = len(guildChannels) - nr_of_categories
 	return nr_of_channels, nr_of_text_channels, nr_of_voice_channels, nr_of_stage_channels, nr_of_categories, nr_of_announcement_channels, nil
 }
 
 func (e *Entity) CountGuildEmojis(guildID string) (int, int, int, error) {
+	log := logger.NewLogrusLogger()
 	nr_of_emojis, nr_of_static_emojis, nr_of_animated_emojis := 0, 0, 0
-
 	guildEmojis, err := e.discord.GuildEmojis(guildID)
 	if err != nil {
+		log.Error(err, "failed to get guild emojis")
 		return 0, 0, 0, nil
 	}
-
 	if len(guildEmojis) == 0 {
+		log.Info("Emojis not exist in current guild")
 		return 0, 0, 0, nil
 	}
-
 	nr_of_emojis = len(guildEmojis)
-
 	for _, emoji := range guildEmojis {
-		if emoji.Animated {
+		// https://discord.com/developers/docs/resources/emoji#list-guild-emojis
+		// Refer to discord doc: true is animated, false is static
+		switch emoji.Animated {
+		case true:
 			nr_of_animated_emojis = nr_of_animated_emojis + 1
-		} else if !emoji.Animated {
+		case false:
+			nr_of_static_emojis = nr_of_static_emojis + 1
+		default:
 			nr_of_static_emojis = nr_of_static_emojis + 1
 		}
 	}
-
 	return nr_of_emojis, nr_of_static_emojis, nr_of_animated_emojis, nil
 }
 
 func (e *Entity) CountGuildStickers(guildID string) (int, int, int, error) {
+	log := logger.NewLogrusLogger()
 	nr_of_stickers, nr_of_standard_stickers, nr_of_guild_stickers := 0, 0, 0
 	url := "https://discord.com/api/v9/guilds/" + guildID + "/stickers"
-
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
+		log.Error(err, "failed to set up request for guild stickers")
 		return 0, 0, 0, err
 	}
-
 	request.Header.Set("Authorization", e.discord.Token)
-
 	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
+		log.Error(err, "failed to get guild stickers")
 		return 0, 0, 0, err
 	}
 	defer resp.Body.Close()
-
 	var guildStickers []response.DiscordGuildSticker
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		log.Error(err, "failed to read guild stickers response")
 		return 0, 0, 0, err
 	}
 	if err := json.Unmarshal(respBody, &guildStickers); err != nil {
+		log.Error(err, "failed to parse guild stickers response")
 		return 0, 0, 0, err
 	}
-
 	if len(guildStickers) == 0 {
+		log.Info("Stickers not exist in current guild")
 		return 0, 0, 0, nil
 	}
-
+	// https://discord.com/developers/docs/resources/sticker#sticker-object-sticker-types
+	// Refer to discord doc: standard sticker is 1, guild sticker is 2
 	for _, sticker := range guildStickers {
-		if sticker.Type == 1 {
+		switch sticker.Type {
+		case consts.StandardSticker:
 			nr_of_standard_stickers = nr_of_standard_stickers + 1
-		} else if sticker.Type == 2 {
+		case consts.GuildSticker:
 			nr_of_guild_stickers = nr_of_guild_stickers + 1
+		default:
+			nr_of_standard_stickers = nr_of_standard_stickers + 1
 		}
 	}
 	nr_of_stickers = len(guildStickers)
@@ -142,8 +157,10 @@ func (e *Entity) CountGuildStickers(guildID string) (int, int, int, error) {
 }
 
 func (e *Entity) CountGuildRoles(guildID string) (int, error) {
+	log := logger.NewLogrusLogger()
 	guildRoles, err := e.discord.GuildRoles(guildID)
 	if err != nil {
+		log.Error(err, "failed to get guild roles")
 		return 0, err
 	}
 
@@ -151,17 +168,17 @@ func (e *Entity) CountGuildRoles(guildID string) (int, error) {
 }
 
 func (e *Entity) CountGuildMembers(guildID string) (int, int, int, error) {
+	log := logger.NewLogrusLogger()
 	nr_of_members, nr_of_user, nr_of_bots := 0, 0, 0
 	members := make([]response.DiscordGuildUser, 0)
-
 	after := ""
 	limit := 1000
 	for {
 		guildMembers, err := e.discord.GuildMembers(guildID, after, limit)
 		if err != nil {
+			log.Error(err, "failed to get guild members")
 			return 0, 0, 0, err
 		}
-
 		for _, member := range guildMembers {
 			members = append(members, response.DiscordGuildUser{
 				User: &response.DiscordUser{
@@ -171,14 +188,11 @@ func (e *Entity) CountGuildMembers(guildID string) (int, int, int, error) {
 				},
 			})
 		}
-
 		if len(guildMembers) < limit {
 			break
 		}
-
 		after = guildMembers[len(guildMembers)-1].User.ID
 	}
-
 	for _, member := range members {
 		if member.User.Bot {
 			nr_of_bots = nr_of_bots + 1
@@ -187,6 +201,5 @@ func (e *Entity) CountGuildMembers(guildID string) (int, int, int, error) {
 		}
 	}
 	nr_of_members = len(members)
-
 	return nr_of_members, nr_of_user, nr_of_bots, nil
 }
