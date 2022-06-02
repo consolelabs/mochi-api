1edefb916184919abe8fe467e88227cb09c91991
NMINHDAI <ngominhdai92tn@gmail.com>
2022-06-02 13:07:39 +0700

    chore: update server and custom stickers

--- migrations/schemas/20220527140133-change-column-name-stickers.sql
+++ migrations/schemas/20220527140133-change-column-name-stickers.sql
@@ -1,9 +1,9 @@
 
 -- +migrate Up
-ALTER TABLE "discord_guild_Stats"
+ALTER TABLE "discord_guild_stats"
     RENAME COLUMN nr_of_guild_stickers TO nr_of_server_stickers;
     
-ALTER TABLE "discord_guild_Stats"
+ALTER TABLE "discord_guild_stats"
     RENAME COLUMN nr_of_standard_stickers TO nr_of_custom_stickers;
 
 -- +migrate Down
