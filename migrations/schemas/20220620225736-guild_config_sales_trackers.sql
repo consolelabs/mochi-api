2a9f79c0d978bdf4ecd665cd5e5310c00f9375af
catng <hoangcat622001@gmail.com>
2022-06-21 15:22:26 +0700

    chore: fix migration

--- migrations/schemas/20220620225736-guild_config_sales_trackers.sql
+++ migrations/schemas/20220620225736-guild_config_sales_trackers.sql
@@ -1,12 +1,6 @@
 
 -- +migrate Up
-CREATE TABLE IF NOT EXISTS guild_config_sales_trackers (
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  guild_id TEXT NOT NULL,
-  channel_id TEXT NOT NULL
-);
-
-CREATE UNIQUE INDEX guild_config_sales_trackers_guild_id_uidx ON guild_config_sales_trackers (guild_id);
+ALTER TABLE guild_config_sales_tracker RENAME TO guild_config_sales_trackers;
 
 -- +migrate Down
-DROP TABLE IF EXISTS guild_config_sales_trackers;
+ALTER TABLE guild_config_sales_trackers RENAME TO guild_config_sales_tracker;
