137931985e338abb739334e849ee211e80f1e531
catng <hoangcat622001@gmail.com>
2022-06-23 14:01:17 +0700

    chore: unit test sales tracker

--- pkg/model/nft_sales_tracker.go
+++ pkg/model/nft_sales_tracker.go
@@ -3,19 +3,20 @@ package model
 import (
 	"github.com/google/uuid"
 )
-type NFTSalesTracker struct{
-	ID        uuid.NullUUID `json:"id" gorm:"default:uuid_generate_v4()"`
-	ContractAddress string `json:"contract_address"`
-	Platform	string `json:"platform"`
-	SalesConfigID []GuildConfigSalesTracker `json:"sales_config_id" gorm:"foreignKey:ID"`
+
+type NFTSalesTracker struct {
+	ID              uuid.NullUUID             `json:"id" gorm:"default:uuid_generate_v4()"`
+	ContractAddress string                    `json:"contract_address"`
+	Platform        string                    `json:"platform"`
+	SalesConfig     []GuildConfigSalesTracker `json:"sales_config_id" gorm:"foreignKey:GuildID"`
 }
 type InsertNFTSalesTracker struct {
-	ID        uuid.NullUUID `json:"id" gorm:"default:uuid_generate_v4()"`
-	ContractAddress string `json:"contract_address"`
-	Platform	string `json:"platform"`
-	SalesConfigID string `json:"sales_config_id" gorm:"foreignKey:ID"`
+	ID              uuid.NullUUID `json:"id" gorm:"default:uuid_generate_v4()"`
+	ContractAddress string        `json:"contract_address"`
+	Platform        string        `json:"platform"`
+	SalesConfigID   string        `json:"sales_config_id" gorm:"foreignKey:ID"`
 }
 
 func (InsertNFTSalesTracker) TableName() string {
 	return "nft_sales_trackers"
-} 
+}
