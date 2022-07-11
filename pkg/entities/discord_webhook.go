3374955ce76efb31c12d59ffe31b1a3b91817169
Ngo Trong Khoi <39881166+trkhoi@users.noreply.github.com>
2022-07-11 22:24:10 +0700

    chore: update missing sales field (#180)

--- pkg/entities/discord_webhook.go
+++ pkg/entities/discord_webhook.go
@@ -11,101 +11,113 @@ import (
 )
 
 func (e *Entity) SendNftSalesToChannel(nftSale request.NftSale) error {
-	data := []*discordgo.MessageEmbed{{
+	data := []*discordgo.MessageEmbedField{
+		{
+			Name:   "Rarity",
+			Value:  e.RarityEmoji(nftSale.Rarity) + " " + nftSale.Rarity,
+			Inline: true,
+		},
+		{
+			Name:   "Rank",
+			Value:  "<:cup:985137841027821589> " + strconv.Itoa(int(nftSale.Rank)),
+			Inline: true,
+		},
+		{
+			Name:   "\u200B",
+			Value:  "\u200B",
+			Inline: true,
+		},
+		{
+			Name:   "Marketplace",
+			Value:  nftSale.Marketplace,
+			Inline: true,
+		},
+		{
+			Name:   "Transaction",
+			Value:  "[" + util.ShortenAddress(nftSale.Transaction) + "]" + "(https://www.youtube.com/)",
+			Inline: true,
+		},
+		{
+			Name:   "\u200B",
+			Value:  "\u200B",
+			Inline: true,
+		},
+		{
+			Name:   "From",
+			Value:  "[" + util.ShortenAddress(nftSale.From) + "]" + "(https://www.youtube.com/)",
+			Inline: true,
+		},
+		{
+			Name:   "To",
+			Value:  "[" + util.ShortenAddress(nftSale.To) + "]" + "(https://www.youtube.com/)",
+			Inline: true,
+		},
+		{
+			Name:   "\u200B",
+			Value:  "\u200B",
+			Inline: true,
+		},
+		{
+			Name:   "Price",
+			Value:  nftSale.Price + " " + strings.ToUpper(nftSale.PaymentToken),
+			Inline: true,
+		},
+		{
+			Name:   "Bought",
+			Value:  nftSale.Bought + " " + strings.ToUpper(nftSale.PaymentToken),
+			Inline: true,
+		},
+		{
+			Name:   "Sold",
+			Value:  nftSale.Sold + " " + strings.ToUpper(nftSale.PaymentToken),
+			Inline: true,
+		},
+	}
+
+	if nftSale.Hodl != "" {
+		dataHodl := discordgo.MessageEmbedField{
+			Name:   "HODL",
+			Value:  nftSale.Hodl,
+			Inline: true,
+		}
+		data = append(data, &dataHodl)
+	}
+
+	if nftSale.Gain != "" {
+		dataGain := discordgo.MessageEmbedField{
+			Name:   "Gain",
+			Value:  nftSale.Gain + " " + strings.ToUpper(nftSale.PaymentToken),
+			Inline: true,
+		}
+		data = append(data, &dataGain)
+	}
+
+	if nftSale.Pnl != "" {
+		dataPnl := discordgo.MessageEmbedField{
+			Name:   "Pnl",
+			Value:  "$" + nftSale.Pnl + " " + "`+" + nftSale.SubPnl + "%`",
+			Inline: true,
+		}
+		data = append(data, &dataPnl)
+	}
+
+	messageSale := []*discordgo.MessageEmbed{{
 		Author: &discordgo.MessageEmbedAuthor{
 			Name:    nftSale.CollectionName,
 			IconURL: nftSale.CollectionImage,
 		},
+		Fields:      data,
 		Description: nftSale.TokenName + " sold!",
 		Color:       int(e.RarityColors(nftSale.Rarity)),
-		Fields: []*discordgo.MessageEmbedField{
-			{
-				Name:   "Rarity",
-				Value:  e.RarityEmoji(nftSale.Rarity) + " " + nftSale.Rarity,
-				Inline: true,
-			},
-			{
-				Name:   "Rank",
-				Value:  "<:cup:985137841027821589> " + strconv.Itoa(int(nftSale.Rank)),
-				Inline: true,
-			},
-			{
-				Name:   "\u200B",
-				Value:  "\u200B",
-				Inline: true,
-			},
-			{
-				Name:   "Marketplace",
-				Value:  nftSale.Marketplace,
-				Inline: true,
-			},
-			{
-				Name:   "Transaction",
-				Value:  "[" + util.ShortenAddress(nftSale.Transaction) + "]" + "(https://www.youtube.com/)",
-				Inline: true,
-			},
-			{
-				Name:   "\u200B",
-				Value:  "\u200B",
-				Inline: true,
-			},
-			{
-				Name:   "From",
-				Value:  "[" + util.ShortenAddress(nftSale.From) + "]" + "(https://www.youtube.com/)",
-				Inline: true,
-			},
-			{
-				Name:   "To",
-				Value:  "[" + util.ShortenAddress(nftSale.To) + "]" + "(https://www.youtube.com/)",
-				Inline: true,
-			},
-			{
-				Name:   "\u200B",
-				Value:  "\u200B",
-				Inline: true,
-			},
-			{
-				Name:   "Price",
-				Value:  nftSale.Price + " " + strings.ToUpper(nftSale.PaymentToken),
-				Inline: true,
-			},
-			{
-				Name:   "Bought",
-				Value:  nftSale.Bought + " " + strings.ToUpper(nftSale.PaymentToken),
-				Inline: true,
-			},
-			{
-				Name:   "Sold",
-				Value:  nftSale.Sold + " " + strings.ToUpper(nftSale.PaymentToken),
-				Inline: true,
-			},
-			{
-				Name:   "HODL",
-				Value:  nftSale.Hodl,
-				Inline: true,
-			},
-			{
-				Name:   "Gain",
-				Value:  nftSale.Gain + " " + strings.ToUpper(nftSale.PaymentToken),
-				Inline: true,
-			},
-			{
-				Name:   "Pnl",
-				Value:  "$" + nftSale.Pnl + " " + "`+" + nftSale.SubPnl + "%`",
-				Inline: true,
-			},
-		},
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
-			URL:    nftSale.TokenImage,
-			Width:  10000,
-			Height: 5000,
+			URL: nftSale.TokenImage,
 		},
 	}}
 	resp, _ := e.GetAllNFTSalesTracker()
 
 	for _, saleChannel := range resp {
 		if nftSale.CollectionAddress == saleChannel.ContractAddress {
-			_, err := e.discord.ChannelMessageSendEmbeds(saleChannel.ChannelID, data)
+			_, err := e.discord.ChannelMessageSendEmbeds(saleChannel.ChannelID, messageSale)
 			if err != nil {
 				e.log.Errorf(err, "[discord.ChannelMessageSendEmbeds] cannot send message to sale channel. CollectionName: %s, TokenName: %s", nftSale.CollectionName, nftSale.TokenName)
 				return fmt.Errorf("cannot send message to sale channel. Error: %v", err)
