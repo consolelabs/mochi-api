f642315a578e8bb345c09512a70003e2f7833bf3
NMINHDAI <ngominhdai92tn@gmail.com>
2022-07-12 17:37:09 +0700

    chore: updating discord webhook UI

--- pkg/entities/discord_webhook.go
+++ pkg/entities/discord_webhook.go
@@ -11,6 +11,14 @@ import (
 )
 
 func (e *Entity) SendNftSalesToChannel(nftSale request.NftSale) error {
+	var urlMarketPlace = ""
+	if nftSale.Marketplace == "Opeasea" {
+		urlMarketPlace = "[" + nftSale.Marketplace + "]" + "(https://opensea.io/assets/ethereum/" + nftSale.CollectionAddress + "/" + nftSale.TokenId + ")"
+	} else if nftSale.Marketplace == "Paintswap" {
+		urlMarketPlace = "[" + nftSale.Marketplace + "]" + "(https://paintswap.finance/marketplace/assets/" + nftSale.CollectionAddress + "/" + nftSale.TokenId + ")"
+	} else { // quixotic
+		urlMarketPlace = "[" + nftSale.Marketplace + "]" + "(https://quixotic.io/asset/" + nftSale.CollectionAddress + "/" + nftSale.TokenId + ")"
+	}
 	data := []*discordgo.MessageEmbedField{
 		{
 			Name:   "Rarity",
@@ -29,7 +37,7 @@ func (e *Entity) SendNftSalesToChannel(nftSale request.NftSale) error {
 		},
 		{
 			Name:   "Marketplace",
-			Value:  "[" + nftSale.Marketplace + "]" + "(https://opensea.io/assets/ethereum/" + nftSale.CollectionAddress + "/" + nftSale.TokenId + ")",
+			Value:  urlMarketPlace,
 			Inline: true,
 		},
 		{
