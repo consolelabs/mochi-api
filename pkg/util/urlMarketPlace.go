a39a2e1fd9f150e23838a45fb0990243b92b347e
NMINHDAI <ngominhdai92tn@gmail.com>
2022-07-12 20:25:31 +0700

    chore: updating discord webhook UI

--- pkg/util/urlMarketPlace.go
+++ pkg/util/urlMarketPlace.go
@@ -1,10 +1,12 @@
 package util
 
+import "strings"
+
 func GetURLMarketPlace(marketplace string) string {
 	var urlMarketPlace = ""
-	if marketplace == "Opensea" {
+	if strings.ToLower(marketplace) == "opensea" {
 		urlMarketPlace = "[" + marketplace + "]" + "(https://opensea.io/assets/ethereum/"
-	} else if marketplace == "Paintswap" {
+	} else if strings.ToLower(marketplace) == "Paintswap" {
 		urlMarketPlace = "[" + marketplace + "]" + "(https://paintswap.finance/marketplace/assets/"
 	} else { // quixotic
 		urlMarketPlace = "[" + marketplace + "]" + "(https://quixotic.io/asset/"
