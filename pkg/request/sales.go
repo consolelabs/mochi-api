02be5274c59c55955176d388fedd7be273e5d426
catng <hoangcat622001@gmail.com>
2022-07-14 17:41:54 +0700

    chore: update webhook

--- pkg/request/sales.go
+++ pkg/request/sales.go
@@ -1,28 +1,33 @@
 package request
 
-type NftSale struct {
-	TokenId           string `json:"token_id"`
-	CollectionAddress string `json:"collection_address"`
-	CollectionName    string `json:"collection_name"`
-	CollectionImage   string `json:"collection_image"`
-	TokenName         string `json:"token_name"`
-	TokenImage        string `json:"token_image"`
-	Rarity            string `json:"rarity"`
-	Rank              uint64 `json:"rank"`
-	Marketplace       string `json:"marketplace"`
-	Transaction       string `json:"transaction"`
-	From              string `json:"from"`
-	To                string `json:"to"`
-	Price             string `json:"price"`
-	Bought            string `json:"bought"`
-	Sold              string `json:"sold"`
-	Hodl              string `json:"hodl"`
-	Gain              string `json:"gain"`
-	Pnl               string `json:"pnl"`
-	SubPnl            string `json:"sub_pnl"`
-	PaymentToken      string `json:"payment_token"`
-}
-
 type NftSalesRequest struct {
-	NftSales []NftSale `json:"nft_sales"`
+	TokenId           string  `json:"token_id"`
+	CollectionAddress string  `json:"collection_address"`
+	CollectionName    string  `json:"collection_name"`
+	CollectionImage   string  `json:"collection_image"`
+	TokenName         string  `json:"token_name"`
+	TokenImage        string  `json:"token_image"`
+	Rarity            string  `json:"rarity"`
+	Rank              uint64  `json:"rank"`
+	Marketplace       string  `json:"marketplace"`
+	Transaction       string  `json:"transaction"`
+	From              string  `json:"from"`
+	To                string  `json:"to"`
+	Price             Token   `json:"price"`
+	Sold              string  `json:"sold"`
+	Hodl              int     `json:"hold"`
+	Gain              Token   `json:"gain"`
+	Pnl               float64 `json:"pnl"`
+	SubPnl            float64 `json:"sub_pnl"`
+	PaymentToken      string  `json:"payment_token"`
+}
+type Token struct {
+	Token  TokenInfo `json:"token"`
+	Amount string    `json:"amount"`
+}
+type TokenInfo struct {
+	Symbol   string `json:"symbol"`
+	IsNative bool   `json:"is_native"`
+	Address  string `json:"address"`
+	Decimal  int    `json:"decimal"`
 }
