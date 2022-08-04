944dc13b7f6159a7e010bbcef1c8ad9a0d2fa200
catng <hoangcat622001@gmail.com>
2022-08-04 12:12:46 +0700

    chore: update hashtag config

--- pkg/request/config.go
+++ pkg/request/config.go
@@ -51,6 +51,7 @@ type ConfigRepostRequest struct {
 }
 
 type TwitterHashtag struct {
+	UserID    string   `json:"user_id"`
 	GuildID   string   `json:"guild_id"`
 	ChannelID string   `json:"channel_id"`
 	Hashtag   []string `json:"hashtag"`
