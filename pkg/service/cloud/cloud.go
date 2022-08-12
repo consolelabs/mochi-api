c742016cae2fd59308f824a3855e0936be2b0ca1
catng <hoangcat622001@gmail.com>
2022-08-12 16:51:03 +0700

    chore: convert webp

--- pkg/service/cloud/cloud.go
+++ pkg/service/cloud/cloud.go
@@ -1,15 +1,19 @@
 package cloud
 
 import (
+	"bytes"
 	"context"
 	"encoding/base64"
 	"fmt"
+	"image/png"
 	"io"
+	"io/ioutil"
 	"os"
 	"strings"
 	"time"
 
 	"cloud.google.com/go/storage"
+	"github.com/chai2010/webp"
 	"github.com/defipod/mochi/pkg/config"
 	"github.com/defipod/mochi/pkg/logger"
 	"github.com/defipod/mochi/pkg/util"
@@ -35,23 +39,34 @@ func NewCloudClient(cfg *config.Config, log logger.Logger) Service {
 }
 
 func (c *clientUploader) HostImageToGCS(imageUrl string, name string) (string, error) {
-	// check if image is .webp
-	if !imageUrlCheck(imageUrl) {
+	needConvert, fromGoogle, isWebp := imageUrlCheck(imageUrl)
+	if !needConvert {
 		return imageUrl, nil
 	}
-
+	fileName := "temp"
 	ctx := context.Background()
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 	defer cancel()
 
 	// download image
-	err := util.DownloadFile(imageUrl, "temp")
+	err := util.DownloadFile(imageUrl, fileName)
 	if err != nil {
 		c.log.Errorf(err, "[cloud.HostImageToGCS] failed to download image: %s", err)
 		return "", fmt.Errorf("[cloud.HostImageToGCS] failed to download image: %s", err)
 	}
-	defer os.Remove("temp")
+	defer os.Remove(fileName)
 	defer os.Remove("resized.png")
+
+	// convert .webp to .png
+	if isWebp {
+		convertedImage, err := webpToPng(fileName)
+		if err != nil {
+			c.log.Errorf(err, "[cloud.HostImageToGCS] failed to convert webp image: %s", err)
+			return "", fmt.Errorf("[cloud.HostImageToGCS] failed to convert webp image: %s", err)
+		}
+		fileName = convertedImage
+		defer os.Remove(convertedImage)
+	}
 
 	// get cloud storage bucket handler
 	handler := c.cl.Bucket(c.bucketName).Object(fmt.Sprintf("%s.png", name))
@@ -59,15 +74,14 @@ func (c *clientUploader) HostImageToGCS(imageUrl string, name string) (string, e
 		c.log.Errorf(err, "[cloud.HostImageToGCS] failed to find bucket %s: %s", c.bucketName, err)
 		return "", fmt.Errorf("[cloud.HostImageToGCS] failed to find storage bucket: %s", err)
 	}
-
 	// open image with imaging package
-	src, err := imaging.Open("temp")
+	src, err := imaging.Open(fileName)
 	if err != nil {
 		c.log.Errorf(err, "[cloud.HostImageToGCS] failed to resize image: %s", err)
 		return "", fmt.Errorf("[cloud.HostImageToGCS] failed to resize image: %s", err)
 	}
 	// resize image if from google and save as png
-	if strings.Contains(imageUrl, "googleusercontent") {
+	if fromGoogle {
 		src = imaging.Resize(src, 300, 0, imaging.Lanczos)
 	}
 	_ = imaging.Save(src, "resized.png")
@@ -91,12 +105,31 @@ func (c *clientUploader) HostImageToGCS(imageUrl string, name string) (string, e
 	return fmt.Sprintf("https://storage.googleapis.com/%s/%s.png", c.bucketName, name), nil
 }
 
-func imageUrlCheck(imageUrl string) bool {
+func imageUrlCheck(imageUrl string) (needConvert bool, fromGoogle bool, isWebp bool) {
 	if strings.Contains(imageUrl, "googleusercontent") {
-		return true
+		return true, true, false
 	}
 	if strings.Contains(imageUrl, ".webp") {
-		return true
+		return true, false, true
 	}
-	return false
+	return false, false, false
+}
+
+func webpToPng(webpFile string) (string, error) {
+	var data []byte
+	var err error
+	out, _ := os.Create("webpConverted.png")
+	// Load file data
+	if data, err = ioutil.ReadFile(webpFile); err != nil {
+		return "", err
+	}
+	m, err := webp.Decode(bytes.NewReader(data))
+	if err != nil {
+		return "", err
+	}
+	err = png.Encode(out, m)
+	if err != nil {
+		return "", err
+	}
+	return "webpConverted.png", nil
 }
