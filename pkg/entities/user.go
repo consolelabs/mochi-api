1ef29b5b8982e392cf076d1a4e982af4c5db13d0
Nguyen Nguyen <nguyennh4522@gmail.com>
2022-08-22 16:37:14 +0700

    fix: unexpected end of JSON input

--- pkg/entities/user.go
+++ pkg/entities/user.go
@@ -143,7 +143,7 @@ func (e *Entity) HandleUserActivities(req *request.HandleUserActivityRequest) (*
 		e.log.Errorf(err, "[HandleUserActivities] - SendLevelUpMessage failed - guild %s, user %s", req.GuildID, req.UserID)
 	} else {
 		e.log.Infof("[HandleUserActivities] - SendLevelUpMessage - userXP: %v, role: %s, res: %v", userXP, role, res)
-		e.svc.Discord.SendLevelUpMessage(userXP.Guild.LogChannel, role, res)
+		e.svc.Discord.SendLevelUpMessage(latestUserXP.Guild.LogChannel, role, res)
 	}
 	return res, nil
 }
