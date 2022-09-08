b05491db45a8a17de80a1984b04c81df63bfdb76
trkhoi <ngotrongkhoi112@gmail.com>
2022-09-08 13:21:58 +0700

    fix: model config nft role

--- pkg/model/guild_config_nft_role.go
+++ pkg/model/guild_config_nft_role.go
@@ -5,8 +5,6 @@ import "github.com/google/uuid"
 type GuildConfigNFTRole struct {
 	ID              uuid.NullUUID `json:"id" gorm:"default:uuid_generate_v4()" swaggertype:"string"`
 	NFTCollectionID uuid.NullUUID `json:"nft_collection_id" swaggertype:"string"`
-	GuildID         string        `json:"guild_id"`
-	RoleID          string        `json:"role_id"`
 	NumberOfTokens  int           `json:"number_of_tokens"`
 	GroupID         uuid.NullUUID `json:"group_id"`
 }
