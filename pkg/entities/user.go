681a3bdcd8b567180061a1cd4d65f28b57b8bc66
nguyennh <104887632+nguyennh4522@users.noreply.github.com>
2022-09-20 16:29:55 +0700

    fix: deleted account response (#361)

--- pkg/entities/user.go
+++ pkg/entities/user.go
@@ -439,6 +439,7 @@ func (e *Entity) GetRoleByGuildLevelConfig(guildID, userID string) (string, erro
 
 func (e *Entity) HandleInviteTracker(inviter *discordgo.Member, invitee *discordgo.Member) (*response.HandleInviteHistoryResponse, error) {
 	res := &response.HandleInviteHistoryResponse{}
+	var guildID string
 
 	if inviter != nil {
 		// create inviter if not exist
@@ -498,20 +499,24 @@ func (e *Entity) HandleInviteTracker(inviter *discordgo.Member, invitee *discord
 		}
 
 		res.InviteeID = invitee.User.ID
+		res.IsInviteeABot = invitee.User.Bot
+		guildID = invitee.GuildID
+	}
 
-		// create invite history
-		inviteType := model.INVITE_TYPE_NORMAL
-		if inviter == nil {
-			inviteType = model.INVITE_TYPE_LEFT
-		}
+	// create invite history
+	inviteType := model.INVITE_TYPE_NORMAL
+	if inviter == nil {
+		inviteType = model.INVITE_TYPE_LEFT
+	}
 
-		// TODO: Can't find age of user now
-		// if time.Now().Unix()-invit < 60*60*24*3 {
-		// 	inviteType = model.INVITE_TYPE_FAKE
-		// }
+	// TODO: Can't find age of user now
+	// if time.Now().Unix()-invit < 60*60*24*3 {
+	// 	inviteType = model.INVITE_TYPE_FAKE
+	// }
 
+	if res.InviteeID != "" && res.InviterID != "" && guildID != "" {
 		if err := e.repo.InviteHistories.Create(&model.InviteHistory{
-			GuildID:   invitee.GuildID,
+			GuildID:   guildID,
 			UserID:    res.InviteeID,
 			InvitedBy: res.InviterID,
 			Type:      inviteType,
