155831494935c27f09fc9924bedf2066fad5ee3c
trkhoi <ngotrongkhoi112@gmail.com>
2022-09-26 17:46:00 +0700

    feat: get nft token transfer

--- pkg/service/indexer/service.go
+++ pkg/service/indexer/service.go
@@ -12,6 +12,7 @@ type Service interface {
 	GetNFTTokens(address, query string) (*response.IndexerGetNFTTokensResponse, error)
 	GetNFTDetail(collectionAddress, tokenID string) (*response.IndexerGetNFTTokenDetailResponse, error)
 	GetNFTActivity(collectionAddress, tokenID, query string) (*response.IndexerGetNFTActivityResponse, error)
+	GetNFTTokenTxHistory(collectionAddress, tokenID string) (*response.IndexerGetNFTTokenTxHistoryResponse, error)
 	GetNftSales(addr string, platform string) (*response.NftSalesResponse, error)
 	GetNFTContract(addr string) (*response.IndexerContract, error)
 	GetNftMetadataAttrIcon() (*response.NftMetadataAttrIconResponse, error)
