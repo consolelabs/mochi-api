cfeeb1520e98c48571a87e68f3ac5fcfdac13fc4
Ngo Trong Khoi <39881166+trkhoi@users.noreply.github.com>
2022-09-27 11:27:45 +0700

    chore: update nft tx history (#386)

--- pkg/response/nft.go
+++ pkg/response/nft.go
@@ -164,15 +164,14 @@ type IndexerGetNFTTokenTxHistoryResponse struct {
 }
 
 type IndexerGetNftTokenTxHistory struct {
-	From            string `json:"from"`
-	To              string `json:"to"`
-	TokenID         string `json:"token_id,omitempty"`
-	EventTime       int64  `json:"event_time"`
-	TransactionHash string `json:"transaction_hash"`
-	BlockNumber     int64  `json:"block_number"`
-	ContractAddress string `json:"contract_address"`
-	CreatedTime     string `json:"created_time"`
-	LastUpdatedTime string `json:"last_updated_time"`
+	From            string    `json:"from"`
+	To              string    `json:"to"`
+	TokenId         string    `json:"token_id"`
+	TransactionHash string    `json:"transaction_hash"`
+	ContractAddress string    `json:"contract_address"`
+	CreatedTime     time.Time `json:"created_time"`
+	ListingStatus   string    `json:"listing_status"`
+	EventType       string    `json:"event_type"`
 }
 
 type IndexerNFTActivityData struct {
