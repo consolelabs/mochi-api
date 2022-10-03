c3cba8024864087c7e8014f24d7c2a81495c8d0a
Nguyen Nguyen <nguyennh4522@gmail.com>
2022-10-03 18:34:26 +0700

    test: community api

--- pkg/model/guild_config_invite_tracker.go
+++ pkg/model/guild_config_invite_tracker.go
@@ -7,6 +7,6 @@ import (
 type GuildConfigInviteTracker struct {
 	ID         uuid.NullUUID  `json:"id" gorm:"default:uuid_generate_v4()" swaggertype:"string"`
 	GuildID    string         `json:"guild_id"`
-	ChannelID  string         `json:"user_id"`
+	ChannelID  string         `json:"channel_id"`
 	WebhookURL JSONNullString `json:"webhook_url"`
 }
