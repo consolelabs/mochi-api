5b80e02f4f506add5e675fe9f40a3659f45b839b
Minh Hoang Ho <49946656+hoangharry@users.noreply.github.com>
2022-10-06 09:45:59 +0700

    test: add test for telegram cmd (#421)

--- pkg/request/telegram.go
+++ pkg/request/telegram.go
@@ -1,6 +1,6 @@
 package request
 
 type LinkUserTelegramWithDiscordRequest struct {
-	TelegramUsername string `json:"telegram_username"`
-	DiscordID        string `json:"discord_id"`
+	TelegramUsername string `json:"telegram_username" binding:"required"`
+	DiscordID        string `json:"discord_id" binding:"required"`
 }
