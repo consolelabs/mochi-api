39bf8863b519fcd985a1c1b8329d79cf27d4f629
catng <hoangcat622001@gmail.com>
2022-10-07 10:45:45 +0700

    chore: test token

--- pkg/handler/configs_test.go
+++ pkg/handler/configs_test.go
@@ -14,77 +14,81 @@ import (
 	"github.com/defipod/mochi/pkg/logger"
 	"github.com/defipod/mochi/pkg/repo/pg"
 	"github.com/defipod/mochi/pkg/request"
+	"github.com/defipod/mochi/pkg/util"
 	"github.com/defipod/mochi/pkg/util/testhelper"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/require"
 )
 
-// import (
-// 	"fmt"
-// 	"io/ioutil"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"net/url"
-// 	"testing"
-
-// 	"github.com/bwmarrin/discordgo"
-// 	"github.com/defipod/mochi/pkg/config"
-// 	"github.com/defipod/mochi/pkg/entities"
-// 	"github.com/defipod/mochi/pkg/logger"
-// 	"github.com/defipod/mochi/pkg/model"
-// 	"github.com/defipod/mochi/pkg/repo/pg"
-// 	"github.com/defipod/mochi/pkg/request"
-// 	"github.com/defipod/mochi/pkg/util"
-// 	"github.com/defipod/mochi/pkg/util/testhelper"
-// 	"github.com/ewohltman/discordgo-mock/mockconstants"
-// 	"github.com/ewohltman/discordgo-mock/mockguild"
-// 	"github.com/ewohltman/discordgo-mock/mockmember"
-// 	"github.com/ewohltman/discordgo-mock/mockrest"
-// 	"github.com/ewohltman/discordgo-mock/mockrole"
-// 	"github.com/ewohltman/discordgo-mock/mocksession"
-// 	"github.com/ewohltman/discordgo-mock/mockstate"
-// 	"github.com/ewohltman/discordgo-mock/mockuser"
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/golang/mock/gomock"
-// 	"github.com/stretchr/testify/require"
-// )
-
-// func TestHandler_GetGuildTokens(t *testing.T) {
+// func TestHandler_ConfigLevelRole(t *testing.T) {
 // 	cfg := config.LoadTestConfig()
 // 	db := testhelper.LoadTestDB("../../migrations/test_seed")
-// 	repo := pg.NewRepo(db)
 // 	log := logger.NewLogrusLogger()
 // 	s := pg.NewPostgresStore(&cfg)
 
 // 	ctrl := gomock.NewController(t)
 // 	defer ctrl.Finish()
+// 	repo := pg.NewRepo(db)
 // 	entityMock := entities.New(cfg, log, repo, s, nil, nil, nil, nil, nil, nil, nil)
 
 // 	tests := []struct {
 // 		name             string
-// 		guildId          string
-// 		wantError        error
+// 		req              request.ConfigLevelRoleRequest
 // 		wantCode         int
+// 		wantError        error
 // 		wantResponsePath string
 // 	}{
-// 		// TODO: Add test cases.
 // 		{
-// 			name:             "success - with guild id",
-// 			guildId:          "863278424433229854",
+// 			name: "success config level role",
+// 			req: request.ConfigLevelRoleRequest{
+// 				GuildID: "895659000996200508",
+// 				RoleID:  "1003867749841383425",
+// 				Level:   5,
+// 			},
 // 			wantCode:         200,
-// 			wantResponsePath: "testdata/guild_config_tokens/200-with-id.json",
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/200-message-ok-uppercase.json",
 // 		},
 // 		{
-// 			name:             "success - without guild id",
-// 			guildId:          "",
+// 			name: "success upsert level role 3",
+// 			req: request.ConfigLevelRoleRequest{
+// 				GuildID: "895659000996200508",
+// 				RoleID:  "1003867749841383426",
+// 				Level:   3,
+// 			},
 // 			wantCode:         200,
-// 			wantResponsePath: "testdata/guild_config_tokens/200-without-id.json",
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/200-message-ok-uppercase.json",
 // 		},
 // 		{
-// 			name:             "success - with wrong guild id",
-// 			guildId:          "123123123",
-// 			wantCode:         200,
-// 			wantResponsePath: "testdata/guild_config_tokens/200-without-id.json",
+// 			name: "fail to config - lack of guildID",
+// 			req: request.ConfigLevelRoleRequest{
+// 				RoleID: "1003867749841383426",
+// 				Level:  3,
+// 			},
+// 			wantCode:         400,
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/400-missing-guildID.json",
+// 		},
+// 		{
+// 			name: "fail to config - lack of roleID",
+// 			req: request.ConfigLevelRoleRequest{
+// 				GuildID: "895659000996200508",
+// 				Level:   3,
+// 			},
+// 			wantCode:         400,
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/400-missing-roleID.json",
+// 		},
+// 		{
+// 			name: "fail to config - lack of level",
+// 			req: request.ConfigLevelRoleRequest{
+// 				GuildID: "895659000996200508",
+// 				RoleID:  "1003867749841383426",
+// 			},
+// 			wantCode:         400,
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/config_level_role/400-missing-level.json",
 // 		},
 // 	}
 // 	for _, tt := range tests {
@@ -95,67 +99,62 @@ import (
 // 			}
 // 			w := httptest.NewRecorder()
 // 			ctx, _ := gin.CreateTestContext(w)
-// 			ctx.Request = httptest.NewRequest("GET", fmt.Sprintf("/api/v1/configs/tokens?guild_id=%s", tt.guildId), nil)
-
-// 			h.GetGuildTokens(ctx)
-
+// 			ctx.Request = httptest.NewRequest("POST", "/api/v1/configs/level-roles", nil)
+// 			util.SetRequestBody(ctx, tt.req)
+// 			h.ConfigLevelRole(ctx)
 // 			require.Equal(t, tt.wantCode, w.Code)
 // 			expRespRaw, err := ioutil.ReadFile(tt.wantResponsePath)
 // 			require.NoError(t, err)
-// 			require.JSONEq(t, string(expRespRaw), w.Body.String(), "[Handler.GetGuildTokens] response mismatched")
+// 			require.JSONEq(t, string(expRespRaw), w.Body.String(), "[Handler.ConfigLevelRole] response mismatched")
 // 		})
 // 	}
 // }
 
-// func TestHandler_UpsertGuildTokenConfig(t *testing.T) {
+// func TestHandler_GetLevelRoleConfigs(t *testing.T) {
 // 	cfg := config.LoadTestConfig()
 // 	db := testhelper.LoadTestDB("../../migrations/test_seed")
-// 	repo := pg.NewRepo(db)
 // 	log := logger.NewLogrusLogger()
 // 	s := pg.NewPostgresStore(&cfg)
 
 // 	ctrl := gomock.NewController(t)
 // 	defer ctrl.Finish()
-// 	//guildConfigToken := mock_guild_config_token.NewMockStore(ctrl)
-// 	//repo.GuildConfigToken = guildConfigToken
+// 	repo := pg.NewRepo(db)
 // 	entityMock := entities.New(cfg, log, repo, s, nil, nil, nil, nil, nil, nil, nil)
+
+// 	type args struct {
+// 		guildID string
+// 	}
 // 	tests := []struct {
 // 		name             string
-// 		req              request.UpsertGuildTokenConfigRequest
-// 		wantError        error
+// 		req              args
 // 		wantCode         int
+// 		wantError        error
 // 		wantResponsePath string
 // 	}{
-// 		// TODO: Add test cases.
 // 		{
-// 			name: "success",
-// 			req: request.UpsertGuildTokenConfigRequest{
-// 				GuildID: "863278424433229854",
-// 				Symbol:  "TOMB",
-// 				Active:  true,
+// 			name: "success get level role",
+// 			req: args{
+// 				guildID: "895659000996200508",
 // 			},
 // 			wantCode:         200,
-// 			wantResponsePath: "testdata/guild_config_tokens/200-create-ok.json",
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/get_level_role_configs/200-success.json",
 // 		},
 // 		{
-// 			name: "fail - invalid request",
-// 			req: request.UpsertGuildTokenConfigRequest{
-// 				GuildID: "",
-// 				Symbol:  "TOMB",
-// 				Active:  true,
-// 			},
+// 			name:             "fail to get - lack of guildID",
+// 			req:              args{},
 // 			wantCode:         400,
-// 			wantResponsePath: "testdata/guild_config_tokens/400-create-invalid-req.json",
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/400-missing-guildID.json",
 // 		},
 // 		{
-// 			name: "fail - cannot find symbol",
-// 			req: request.UpsertGuildTokenConfigRequest{
-// 				GuildID: "863278424433229854",
-// 				Symbol:  "TOMBA",
-// 				Active:  true,
+// 			name: "no data to get",
+// 			req: args{
+// 				guildID: "abc",
 // 			},
-// 			wantCode:         500,
-// 			wantResponsePath: "testdata/guild_config_tokens/500-create-symbol-not-found.json",
+// 			wantCode:         200,
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/get_level_role_configs/200-no-data.json",
 // 		},
 // 	}
 // 	for _, tt := range tests {
@@ -166,17 +165,95 @@ import (
 // 			}
 // 			w := httptest.NewRecorder()
 // 			ctx, _ := gin.CreateTestContext(w)
-// 			ctx.Request = httptest.NewRequest("POST", "/api/v1/configs/tokens", nil)
-// 			util.SetRequestBody(ctx, tt.req)
-// 			// guildConfigToken.EXPECT().UpsertMany([]model.GuildConfigToken{{GuildID: tt.req.GuildID, TokenID: tt.req.Symbol}}).Return()
-
-// 			h.UpsertGuildTokenConfig(ctx)
-
+// 			ctx.Params = []gin.Param{
+// 				{
+// 					Key:   "guild_id",
+// 					Value: tt.req.guildID,
+// 				},
+// 			}
+// 			ctx.Request = httptest.NewRequest("GET", "/api/v1/configs/level-roles/", nil)
+// 			h.GetLevelRoleConfigs(ctx)
 // 			require.Equal(t, tt.wantCode, w.Code)
 // 			expRespRaw, err := ioutil.ReadFile(tt.wantResponsePath)
 // 			require.NoError(t, err)
-// 			require.JSONEq(t, string(expRespRaw), w.Body.String(), "[Handler.GetGuildTokens] response mismatched")
+// 			require.JSONEq(t, string(expRespRaw), w.Body.String(), "[Handler.GetLevelRoleConfigs] response mismatched")
+// 		})
+// 	}
+// }
 
+// func TestHandler_RemoveLevelRoleConfig(t *testing.T) {
+// 	cfg := config.LoadTestConfig()
+// 	db := testhelper.LoadTestDB("../../migrations/test_seed")
+// 	log := logger.NewLogrusLogger()
+// 	s := pg.NewPostgresStore(&cfg)
+
+// 	ctrl := gomock.NewController(t)
+// 	defer ctrl.Finish()
+// 	repo := pg.NewRepo(db)
+// 	entityMock := entities.New(cfg, log, repo, s, nil, nil, nil, nil, nil, nil, nil)
+
+// 	type args struct {
+// 		guildID string
+// 		level   string
+// 	}
+
+// 	tests := []struct {
+// 		name             string
+// 		req              args
+// 		wantCode         int
+// 		wantError        error
+// 		wantResponsePath string
+// 	}{
+// 		{
+// 			name: "success delete config level role",
+// 			req: args{
+// 				guildID: "895659000996200508",
+// 				level:   "2",
+// 			},
+// 			wantCode:         200,
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/200-message-ok-uppercase.json",
+// 		},
+// 		{
+// 			name:             "fail to delete level role - lack of guildid",
+// 			req:              args{},
+// 			wantCode:         400,
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/400-missing-guildID.json",
+// 		},
+// 		{
+// 			name: "fail to delete level role - lack of level",
+// 			req: args{
+// 				guildID: "895659000996200508",
+// 			},
+// 			wantCode:         400,
+// 			wantError:        nil,
+// 			wantResponsePath: "testdata/remove_level_role_config/400-missing-level.json",
+// 		},
+// 	}
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			h := &Handler{
+// 				entities: entityMock,
+// 				log:      log,
+// 			}
+// 			w := httptest.NewRecorder()
+// 			ctx, _ := gin.CreateTestContext(w)
+// 			ctx.Params = []gin.Param{
+// 				{
+// 					Key:   "guild_id",
+// 					Value: tt.req.guildID,
+// 				},
+// 			}
+// 			q := url.Values{}
+// 			q.Add("level", tt.req.level)
+// 			ctx.Request = httptest.NewRequest("DELETE", "/api/v1/configs/level-roles/", nil)
+// 			ctx.Request.URL.RawQuery = q.Encode()
+// 			h.RemoveLevelRoleConfig(ctx)
+// 			require.Equal(t, tt.wantCode, w.Code)
+// 			expRespRaw, err := ioutil.ReadFile(tt.wantResponsePath)
+// 			require.NoError(t, err)
+// 			require.JSONEq(t, string(expRespRaw), w.Body.String(), "[Handler.ConfigLevelRole] response mismatched")
 // 		})
 // 	}
 // }
@@ -810,6 +887,152 @@ func TestHandler_DeleteVoteChannelConfig(t *testing.T) {
 			require.NoError(t, err)
 
 			require.JSONEq(t, string(expRespRaw), w.Body.String(), "[Handler.DeleteVoteChannelConfig] response mismatched")
+		})
+	}
+}
+
+func TestHandler_GetGuildTokens(t *testing.T) {
+	cfg := config.LoadTestConfig()
+	db := testhelper.LoadTestDB("../../migrations/test_seed")
+	repo := pg.NewRepo(db)
+	log := logger.NewLogrusLogger()
+	s := pg.NewPostgresStore(&cfg)
+
+	entityMock := entities.New(cfg, log, repo, s, nil, nil, nil, nil, nil, nil, nil)
+	h := &Handler{
+		entities: entityMock,
+		log:      log,
+	}
+	tests := []struct {
+		name             string
+		guildId          string
+		wantError        error
+		wantCode         int
+		wantResponsePath string
+	}{
+		// TODO: Add test cases.
+		{
+			name:             "success - with guild id",
+			guildId:          "863278424433229854",
+			wantCode:         200,
+			wantResponsePath: "testdata/guild_config_tokens/200-with-id.json",
+		},
+		{
+			name:             "success - without guild id",
+			guildId:          "",
+			wantCode:         200,
+			wantResponsePath: "testdata/guild_config_tokens/200-without-id.json",
+		},
+		{
+			name:             "success - with wrong guild id",
+			guildId:          "123123123",
+			wantCode:         200,
+			wantResponsePath: "testdata/guild_config_tokens/200-without-id.json",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+			ctx.Request = httptest.NewRequest("GET", fmt.Sprintf("/api/v1/configs/tokens?guild_id=%s", tt.guildId), nil)
+
+			h.GetGuildTokens(ctx)
+
+			require.Equal(t, tt.wantCode, w.Code)
+			expRespRaw, err := ioutil.ReadFile(tt.wantResponsePath)
+			require.NoError(t, err)
+			require.JSONEq(t, string(expRespRaw), w.Body.String(), "[Handler.GetGuildTokens] response mismatched")
+		})
+	}
+}
+
+func TestHandler_UpsertGuildTokenConfig(t *testing.T) {
+	cfg := config.LoadTestConfig()
+	db := testhelper.LoadTestDB("../../migrations/test_seed")
+	repo := pg.NewRepo(db)
+	log := logger.NewLogrusLogger()
+	s := pg.NewPostgresStore(&cfg)
+
+	entityMock := entities.New(cfg, log, repo, s, nil, nil, nil, nil, nil, nil, nil)
+	h := &Handler{
+		entities: entityMock,
+		log:      log,
+	}
+	tests := []struct {
+		name             string
+		req              request.UpsertGuildTokenConfigRequest
+		wantError        error
+		wantCode         int
+		wantResponsePath string
+	}{
+		{
+			name: "success - add - new token",
+			req: request.UpsertGuildTokenConfigRequest{
+				GuildID: "863278424433229854",
+				Symbol:  "TOMB",
+				Active:  true,
+			},
+			wantError:        nil,
+			wantCode:         200,
+			wantResponsePath: "testdata/200-message-ok.json",
+		},
+		{
+			name: "success - add - same token",
+			req: request.UpsertGuildTokenConfigRequest{
+				GuildID: "863278424433229854",
+				Symbol:  "FTM",
+				Active:  true,
+			},
+			wantError:        nil,
+			wantCode:         200,
+			wantResponsePath: "testdata/200-message-ok.json",
+		},
+		{
+			name: "success - remove - existed token",
+			req: request.UpsertGuildTokenConfigRequest{
+				GuildID: "863278424433229854",
+				Symbol:  "SHIB",
+				Active:  false,
+			},
+			wantError:        nil,
+			wantCode:         200,
+			wantResponsePath: "testdata/200-message-ok.json",
+		},
+		{
+			name: "failed - remove - non existed token",
+			req: request.UpsertGuildTokenConfigRequest{
+				GuildID: "863278424433229854",
+				Symbol:  "ABCC",
+				Active:  false,
+			},
+			wantError:        nil,
+			wantCode:         200,
+			wantResponsePath: "testdata/404_record_not_found.json",
+		},
+		{
+			name: "failed - missing guild",
+			req: request.UpsertGuildTokenConfigRequest{
+				GuildID: "",
+				Symbol:  "ABCC",
+				Active:  false,
+			},
+			wantError:        nil,
+			wantCode:         200,
+			wantResponsePath: "testdata/400-missing-guildID.json",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+			ctx.Request = httptest.NewRequest("POST", "/api/v1/configs/token", nil)
+			util.SetRequestBody(ctx, tt.req)
+
+			h.UpsertGuildTokenConfig(ctx)
+			expRespRaw, err := ioutil.ReadFile(tt.wantResponsePath)
+			require.NoError(t, err)
+			require.JSONEq(t, string(expRespRaw), w.Body.String(), "[Handler.UpsertGuildTokenConfig] response mismatched")
 		})
 	}
 }
