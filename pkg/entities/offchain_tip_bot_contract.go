9272138b4938b82657793c2321ef6035a1de062b
trkhoi <ngotrongkhoi112@gmail.com>
2022-10-19 20:58:14 +0700

    chore: return exist assigned contract
    chore: update swagger

--- pkg/entities/offchain_tip_bot_contract.go
+++ pkg/entities/offchain_tip_bot_contract.go
@@ -4,7 +4,7 @@ import (
 	"github.com/defipod/mochi/pkg/model"
 )
 
-func (e *Entity) OffchainTipBotCreateAssignContract(ac *model.OffchainTipBotAssignContract) (err error) {
+func (e *Entity) OffchainTipBotCreateAssignContract(ac *model.OffchainTipBotAssignContract) (userAssignedContract *model.OffchainTipBotAssignContract, err error) {
 	return e.repo.OffchainTipBotContract.CreateAssignContract(ac)
 }
 
