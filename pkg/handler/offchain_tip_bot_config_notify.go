1cec6e2c518c9f15dd4120eb7890ce4f97ba1451
trkhoi <ngotrongkhoi112@gmail.com>
2022-11-29 14:42:22 +0700

    feat: config notify for tip

---
+++ pkg/handler/offchain_tip_bot_config_notify.go
@@ -0,0 +1,15 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) CreateConfigNotify(c *gin.Context) {
+
+}
+
+func (h *Handler) ListConfigNotify(c *gin.Context) {
+
+}
+
+func (h *Handler) DeleteConfigNotify(c *gin.Context) {
+
+}
