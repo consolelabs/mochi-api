bf257c75f0cabc5c94e07da41229eff195ec5d88
trkhoi <ngotrongkhoi112@gmail.com>
2022-12-22 16:07:56 +0700

    chore: add collection with priority

--- pkg/entities/nfts.go
+++ pkg/entities/nfts.go
@@ -369,11 +369,12 @@ func (e *Entity) CreateSolanaNFTCollection(req request.CreateNFTCollectionReques
 	}
 
 	err = e.indexer.CreateERC721Contract(indexer.CreateERC721ContractRequest{
-		Address:   solanaCollection.Data.Data.CollectionId,
-		ChainID:   0,
-		Name:      solanaCollection.Data.Data.Collection,
-		Symbol:    solanaCollection.Data.Data.Symbol,
-		MessageID: req.MessageID,
+		Address:      solanaCollection.Data.Data.CollectionId,
+		ChainID:      0,
+		Name:         solanaCollection.Data.Data.Collection,
+		Symbol:       solanaCollection.Data.Data.Symbol,
+		MessageID:    req.MessageID,
+		PriorityFlag: req.PriorityFlag,
 	})
 	if err != nil {
 		e.log.Errorf(err, "[CreateERC721Contract] failed to create erc721 contract: %v", err)
@@ -476,10 +477,11 @@ func (e *Entity) CreateEVMNFTCollection(req request.CreateNFTCollectionRequest) 
 	}
 
 	err = e.indexer.CreateERC721Contract(indexer.CreateERC721ContractRequest{
-		Address: req.Address,
-		ChainID: chainID,
-		Name:    name,
-		Symbol:  symbol,
+		Address:      req.Address,
+		ChainID:      chainID,
+		Name:         name,
+		Symbol:       symbol,
+		PriorityFlag: req.PriorityFlag,
 	})
 	if err != nil {
 		e.log.Errorf(err, "[CreateERC721Contract] failed to create erc721 contract: %v", err)
