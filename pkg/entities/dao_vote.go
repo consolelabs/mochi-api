ef23130191eff94a1bcc7c5f5a8a24df0b1640cc
Minh Hoang Ho <49946656+hoangharry@users.noreply.github.com>
2023-01-03 16:51:57 +0700

    feat: create dao vote (#609)

--- pkg/entities/dao_vote.go
+++ pkg/entities/dao_vote.go
@@ -1,1 +1,15 @@
 package entities
+
+import (
+	"github.com/defipod/mochi/pkg/model"
+	"github.com/defipod/mochi/pkg/request"
+)
+
+func (e *Entity) CreateDaoVote(req request.CreateDaoVoteRequest) error {
+	return e.repo.DaoVote.CreateDaoVote(&model.DaoVote{
+		UserId:     req.UserID,
+		ProposalId: req.ProposalID,
+		Choice:     req.Choice,
+		Point:      1,
+	})
+}
--- pkg/request/dao_voting.go
+++ pkg/request/dao_voting.go
@@ -3,3 +3,9 @@ package request
 type DeleteGuildConfigDaoProposal struct {
 	ID string `json:"id"`
 }
+
+type CreateDaoVoteRequest struct {
+	UserID     string `json:"user_id" binding:"required"`
+	ProposalID int64  `json:"proposal_id" binding:"required"`
+	Choice     string `json:"choice" binding:"required"`
+}
