0a96c8ac07df854614a8429132468e5c36bcd7f8
phucledien <phucledien@gmail.com>
2023-03-17 13:55:44 +0700

    fix: tokenrole fetch user address by mochi profile

--- pkg/repo/user_token_balance/store.go
+++
@@ -1,8 +0,0 @@
-package user_token_balance
-
-import "github.com/defipod/mochi/pkg/model"
-
-type Store interface {
-	Upsert(balance model.UserTokenBalance) error
-	GetUserTokenBalancesByUserInGuild(guildID string) ([]model.MemberTokenRole, error)
-}
