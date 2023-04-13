74fac21a0850c8e40d230dcbdb17169db94c62f6
nguyenhuy1812 <nguyenhuy1812@gmail.com>
2023-04-13 15:54:14 +0700

    chore: enhance airdrop offchain transfer

--- pkg/consts/offchain_tip_bot.go
+++ pkg/consts/offchain_tip_bot.go
@@ -5,8 +5,10 @@ const (
 	OffchainTipBotContractAssignStatusAvailable    = 1
 	OffchainTipBotTrasferStatusSuccess             = "success"
 	OffchainTipBotTrasferStatusFail                = "fail"
+	OffchainTipBotFailReasonGetProfileFailed       = "Get profile failed"
 	OffchainTipBotFailReasonNotEnoughBalance       = "Not enough balance"
 	OffchainTipBotFailReasonTokenNotSupported      = "Token not supported"
 	OffchainTipBotFailReasonInvalidAmount          = "Invalid amount"
+	OffchainTipBotFailReasonMochiPayTransferFailed = "Mochi Pay transfer failed"
 	OffchainTipBotTransferTypeTip                  = "tip"
 )
--- pkg/service/mochipay/service.go
+++ pkg/service/mochipay/service.go
@@ -1,8 +1,11 @@
 package mochipay
 
-import "github.com/defipod/mochi/pkg/request"
+import (
+	"github.com/defipod/mochi/pkg/request"
+)
 
 type Service interface {
 	SwapMochiPay(req request.KyberSwapRequest) error
 	GetBalance(profileId, token string) (*GetBalanceDataResponse, error)
+	Transfer(req request.MochiPayTransferRequest) error
 }
