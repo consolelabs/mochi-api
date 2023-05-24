9f65858f6705ea4c6761f3fdff1dfb351f87e3ca
Ngo Trong Khoi <39881166+trkhoi@users.noreply.github.com>
2023-05-24 18:14:35 +0700

    fix: vault list no amount (#858)

--- pkg/entities/vault.go
+++ pkg/entities/vault.go
@@ -113,20 +113,22 @@ func (e *Entity) GetVaults(req request.GetVaultsRequest) ([]model.Vault, error) 
 		return nil, err
 	}
 
-	for i, vault := range vaults {
-		walletAssetsEVM, err := e.ListWalletAssets(request.ListWalletAssetsRequest{Type: "eth", Address: vault.WalletAddress})
-		if err != nil {
-			e.log.Fields(logger.Fields{"vault": vault}).Errorf(err, "[entity.GetVaults] e.ListWalletAssets() failed")
-			return nil, err
-		}
-		vaults[i].TotalAmountEVM = fmt.Sprintf("%.4f", sumBal(walletAssetsEVM))
+	if req.NoAmount != "true" {
+		for i, vault := range vaults {
+			walletAssetsEVM, err := e.ListWalletAssets(request.ListWalletAssetsRequest{Type: "eth", Address: vault.WalletAddress})
+			if err != nil {
+				e.log.Fields(logger.Fields{"vault": vault}).Errorf(err, "[entity.GetVaults] e.ListWalletAssets() failed")
+				return nil, err
+			}
+			vaults[i].TotalAmountEVM = fmt.Sprintf("%.4f", sumBal(walletAssetsEVM))
 
-		walletAssetsSolana, err := e.ListWalletAssets(request.ListWalletAssetsRequest{Type: "sol", Address: vault.SolanaWalletAddress})
-		if err != nil {
-			e.log.Fields(logger.Fields{"vault": vault}).Errorf(err, "[entity.GetVaults] e.ListWalletAssets() failed")
-			return nil, err
+			walletAssetsSolana, err := e.ListWalletAssets(request.ListWalletAssetsRequest{Type: "sol", Address: vault.SolanaWalletAddress})
+			if err != nil {
+				e.log.Fields(logger.Fields{"vault": vault}).Errorf(err, "[entity.GetVaults] e.ListWalletAssets() failed")
+				return nil, err
+			}
+			vaults[i].TotalAmountSolana = fmt.Sprintf("%.4f", sumBal(walletAssetsSolana))
 		}
-		vaults[i].TotalAmountSolana = fmt.Sprintf("%.4f", sumBal(walletAssetsSolana))
 	}
 
 	return vaults, nil
