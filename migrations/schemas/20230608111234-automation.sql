667333a7c362e77e4d69551be79b32fbb983a855
Thành Phạm Ngọc <ngocthanh2207@gmail.com>
2023-06-16 11:09:20 +0700

    feat: draft version of auto trigger for trigger react

---
+++ migrations/schemas/20230608111234-automation.sql
@@ -0,0 +1,142 @@
+
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS auto_triggers
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    guild_id TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT TRUE,
+    updated_at timestamp with time zone not null default now(),
+    created_at timestamp with time zone default now()
+);
+
+CREATE TABLE IF NOT EXISTS auto_conditions
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    trigger_id TEXT NOT NULL,
+    type_id TEXT NOT NULL,
+    channel_id TEXT NULL,
+    index INTEGER NOT NULL,
+    platform TEXT NOT NULL,
+    child_id TEXT NULL,
+    updated_at timestamp with time zone not null default now(),
+    created_at timestamp with time zone default now()
+);
+
+CREATE TABLE IF NOT EXISTS auto_condition_values
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    condition_id TEXT NOT NULL,
+    child_id TEXT NULL,
+    type TEXT NOT NULL,
+    index INTEGER NOT NULL,
+    operator TEXT NOT NULL,
+    matches TEXT NOT NULL,
+    created_at timestamp with time zone default now()
+);
+
+CREATE TABLE IF NOT EXISTS auto_condition_types
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    icon_url TEXT NOT NULL,
+    created_at timestamp with time zone default now()
+);
+
+CREATE TABLE IF NOT EXISTS auto_condition_type_presets
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    type_id TEXT NOT NULL,
+    value TEXT NOT NULL,
+    created_at timestamp with time zone default now()
+);
+
+CREATE TABLE IF NOT EXISTS auto_actions
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    user_ids TEXT NULL,
+    trigger_id TEXT NOT NULL,
+    type_id TEXT NOT NULL,
+    channel_ids NULL,
+    index INTEGER NOT NULL,
+    action_data TEXT NOT NULL,
+    name TEXT NOT NULL,
+    content TEXT NULL,
+    embed_id TEXT NULL,
+    then_action_id TEXT NULL,
+    created_at timestamp with time zone default now()
+);
+
+CREATE TABLE IF NOT EXISTS auto_action_types
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    icon_url TEXT NOT NULL,
+    created_at timestamp with time zone default now()
+);
+
+CREATE TABLE IF NOT EXISTS auto_embeds
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    action_id TEXT NOT NULL,
+    author_id TEXT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    color TEXT NOT NULL,
+    image_id TEXT NULL,
+    thumbnail_id TEXT NULL,
+    footer_id TEXT NULL,
+    video_id TEXT NULL,
+    url TEXT NULL,
+    type TEXT NULL,
+    fields TEXT NULL,
+    created_at timestamp with time zone default now()
+);
+
+CREATE TABLE IF NOT EXISTS auto_embed_images
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    embed_id TEXT NOT NULL,
+    url TEXT NOT NULL,
+    proxy_url TEXT NOT NULL,
+    height INTEGER NOT NULL,
+    width INTEGER NOT NULL,
+    created_at timestamp with time zone default now()
+);
+
+CREATE TABLE IF NOT EXISTS auto_embed_videos
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    embed_id TEXT NOT NULL,
+    url TEXT NOT NULL,
+    height INTEGER NOT NULL,
+    width INTEGER NOT NULL,
+    created_at timestamp with time zone default now()
+);
+
+CREATE TABLE IF NOT EXISTS auto_embed_footers
+(
+    id uuid DEFAULT uuid_generate_v4(),
+    embed_id TEXT NOT NULL,
+    text TEXT NOT NULL,
+    icon_url TEXT NOT NULL,
+    url TEXT NOT NULL,
+    created_at timestamp with time zone default now()
+);
+
+
+-- +migrate Down
+DROP TABLE IF EXISTS auto_triggers;
+DROP TABLE IF EXISTS auto_conditions;
+DROP TABLE IF EXISTS auto_condition_values;
+DROP TABLE IF EXISTS auto_condition_types;
+DROP TABLE IF EXISTS auto_condition_type_presets;
+DROP TABLE IF EXISTS auto_actions;
+DROP TABLE IF EXISTS auto_action_types;
+DROP TABLE IF EXISTS auto_embeds;
+DROP TABLE IF EXISTS auto_embed_images;
+DROP TABLE IF EXISTS auto_embed_videos;
+DROP TABLE IF EXISTS auto_embed_footers;
---
+++ migrations/seed/auto_trigger.sql
@@ -0,0 +1,62 @@
+
+
+INSERT INTO public.auto_triggers(
+	id, guild_id, user_id, name, status, updated_at, created_at)
+	VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24bb', '711823851117608990', '711823390000021556', 'Message', true, now(), now());
+
+INSERT INTO public.auto_triggers(id, guild_id, user_id, name, status, updated_at, created_at) VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24ba', '711823851117608990', '711823390000021556', 'React in general', true, now(), now());
+
+INSERT INTO public.auto_types(id, name, type, icon_url, created_at) VALUES ('2236b7a8-9f9c-456b-a5fa-2dc0755d24b1', 'create message', 'createMessage', '', now());
+INSERT INTO public.auto_types(id, name, type, icon_url, created_at) VALUES ('2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', 'React message', 'reactionAdd', '', now());
+INSERT INTO public.auto_types(id, name, type, icon_url, created_at) VALUES ('2236b7a8-9f9c-456b-a5fa-2dc0755d24b3', 'Total react', 'totalReact', '', now());
+INSERT INTO public.auto_types(id, name, type, icon_url, created_at) VALUES ('2236b7a8-9f9c-456b-a5fa-2dc0755d24b4', 'Total message', 'totalMessage', '', now());
+INSERT INTO public.auto_types(id, name, type, icon_url, created_at) VALUES ('2236b7a8-9f9c-456b-a5fa-2dc0755d24b5', 'Message in channel', 'messageChannel', '', now());
+INSERT INTO public.auto_types(id, name, type, icon_url, created_at) VALUES ('2236b7a8-9f9c-456b-a5fa-2dc0755d24b6', 'React in channel', 'reactChannel', '', now());
+INSERT INTO public.auto_types(id, name, type, icon_url, created_at) VALUES ('2236b7a8-9f9c-456b-a5fa-2dc0755d24b7', 'user role', 'userRole', '', now());
+INSERT INTO public.auto_types(id, name, type, icon_url, created_at) VALUES ('2236b7a8-9f9c-456b-a5fa-2dc0755d24b8', 'action send message', 'sendMessage', '', now());
+INSERT INTO public.auto_types(id, name, type, icon_url, created_at) VALUES ('2236b7a8-9f9c-456b-a5fa-2dc0755d24b9', 'React type', 'reactType', '', now());
+
+
+
+INSERT INTO public.auto_actions(
+	id, user_ids, trigger_id, type_id, channel_ids, name, content, created_at)
+	VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', '7236b7a8-9f9c-456b-a5fa-2dc0755d24bb', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 2', 'Hello world', now());
+    VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', '7236b7a8-9f9c-456b-a5fa-2dc0755d24bb', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 2', 'Hello world', now());
+    VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', '7236b7a8-9f9c-456b-a5fa-2dc0755d24bb', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 2', 'Hello world', now());
+    VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', '7236b7a8-9f9c-456b-a5fa-2dc0755d24bb', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 2', 'Hello world', now());
+    VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', '7236b7a8-9f9c-456b-a5fa-2dc0755d24bb', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 2', 'Hello world', now());
+
+
+INSERT INTO public.auto_actions(
+	id, user_ids, trigger_id, type_id, channel_ids, name, content, created_at)
+	VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '', '7236b7a8-9f9c-456b-a5fa-2dc0755d24ba', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 2', 'Hello world', now());
+
+INSERT INTO public.auto_actions(
+	id, user_ids, trigger_id, type_id, channel_ids, name, content, created_at)
+	VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b4', '', '7236b7a8-9f9c-456b-a5fa-2dc0755d24ba', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 3', 'Hello world 3', now());
+
+INSERT INTO public.auto_conditions(
+	id, trigger_id, type_id, channel_id, index, platform, updated_at, created_at)
+	VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '7236b7a8-9f9c-456b-a5fa-2dc0755d24ba', '7236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '711823851117608992', 1, 'discord', now(), now());
+
+
+INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches)
+	VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '7236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b7', 2, 'in', now(), now());
+
+INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches)
+	VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24b7', '7236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b3', 2, '==', now(), "<:pepeheart:867454854686048256>");
+INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches)
+	VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24b6', '7236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b3', 3, '==', now(), "3");
+
+
+INSERT INTO public.auto_embed_image(id, embed_id, url, proxy_url, height, width, created_at)
+	VALUES ('fb64b906-7e0c-4162-b79d-2f0690b543da', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 100, 100, now());
+    VALUES ('9e41792b-193b-48e0-98e1-478875020586', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 100, 100, now());
+    VALUES ('b4dbe5b9-2091-40ce-8862-ddc4b1785179', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 100, 100, now());
+
+INSERT INTO public.auto_embed_footer(id, text, icon_url, url, created_at)
+	VALUES ('e0a1870a-8cfc-407d-9492-e8a45c0d613d', 'Hello footer', 'icon', 'url', now());
+    VALUES ('88003cd5-724d-48bc-859a-197e6c0a3b59', 'Hello footer', 'icon', 'url', now());
+
+INSERT INTO public.auto_embed_video(id, url, height, width, created_at)
+	VALUES (uuid_generate_v4(), 'video url 1', 110, 50, now());
