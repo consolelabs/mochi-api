cb5447101c61085ca5c314c632761d6b581b1d3b
Thành Phạm Ngọc <ngocthanh2207@gmail.com>
2023-06-20 14:46:04 +0700

    update: update seed script for automation and migrate table name

--- migrations/seed/auto_trigger.sql
+++ migrations/seed/auto_trigger.sql
@@ -15,9 +15,9 @@ INSERT INTO public.auto_triggers(id, guild_id, user_id, name, status, updated_at
 INSERT INTO public.auto_triggers(id, guild_id, user_id, name, status, updated_at, created_at) VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24ba', '711823851117608990', '711823390000021556', 'React heart in general', true, now(), now());
 
 -- Seed data 
-INSERT INTO public.auto_actions(id, user_ids, trigger_id, type_id, channel_ids, name, content, created_at) VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', '7236b7a8-9f9c-456b-a5fa-2dc0755d24bb', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 1', 'Hello world', now());
-INSERT INTO public.auto_actions(id, user_ids, trigger_id, type_id, channel_ids, name, content, created_at) VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '', '7236b7a8-9f9c-456b-a5fa-2dc0755d24ba', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 2', 'Hello world', now());
-INSERT INTO public.auto_actions(id, user_ids, trigger_id, type_id, channel_ids, name, content, created_at) VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b4', '', '7236b7a8-9f9c-456b-a5fa-2dc0755d24ba', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 3', 'Hello world 3', now());
+INSERT INTO public.auto_actions(id, trigger_id, type_id, channel_ids, name, content, action_data, created_at) VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '7236b7a8-9f9c-456b-a5fa-2dc0755d24bb', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 1', 'Hello world', '', now());
+INSERT INTO public.auto_actions(id, trigger_id, type_id, channel_ids, name, content, action_data, created_at) VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '7236b7a8-9f9c-456b-a5fa-2dc0755d24ba', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '', 'Do action 2', 'Hello world', '', now());
+INSERT INTO public.auto_actions(id,  trigger_id, type_id, channel_ids, name, content, action_data, created_at) VALUES ('4236b7a8-9f9c-456b-a5fa-2dc0755d24b4','7236b7a8-9f9c-456b-a5fa-2dc0755d24ba', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b8', '', 'Do action 3', 'Hello world 3', '', now());
 
 -- Seed data condition
 INSERT INTO public.auto_conditions(id, trigger_id, type_id, channel_id, index, platform, updated_at, created_at) VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '7236b7a8-9f9c-456b-a5fa-2dc0755d24ba', '7236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '711823851117608992', 1, 'discord', now(), now());
@@ -27,20 +27,16 @@ INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, opera
 INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches) VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '7236b7a8-9f9c-456b-a5fa-2dc0755d24b2', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b7', 2, 'in', now(), '');
 INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches) VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24b5', '7236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b7', 1, 'in', now(), '1115906135799648257,711823851117608990');
 INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches) VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24b7', '7236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b9', 2, '==', now(), "<:pepeheart:867454854686048256>");
-INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches) VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24b6', '7236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b3', 3, '==', now(), "3");
+INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches) VALUES ('7236b7a8-9f9c-456b-a5fa-2dc0755d24b6', '7236b7a8-9f9c-456b-a5fa-2dc0755d24b3', '2236b7a8-9f9c-456b-a5fa-2dc0755d24b3', 3, '==', now(), "1");
 
-INSERT INTO public.auto_embeds(
-	id, author_id, title, description, color, image_id, thumbnail_id, footer_id, video_id, url, type, fields, created_at, action_id)
-	VALUES ('d95d4370-2629-4d85-85ea-b8d623dbaff4', 'fbaca19d-4ecc-4627-92b6-81858536f921', 'Embed 1', 'descript 1', '#000000', ?, ?, ?, ?, ?, ?, ?, ?, ?);
+INSERT INTO public.auto_embeds(id, author_id, action_id, title, description, color, url, type, fields, created_at)
+	VALUES ('d95d4370-2629-4d85-85ea-b8d623dbaff4', 'fbaca19d-4ecc-4627-92b6-81858536f921', '4236b7a8-9f9c-456b-a5fa-2dc0755d24b3', 'Embed 1', 'descript 1', '#000000', 'https://openai.com/research/measuring-goodharts-law', 'some types', 'any fields', now(), now());
 
-INSERT INTO public.auto_embed_image(id, embed_id, url, proxy_url, height, width, created_at)
-	VALUES ('fb64b906-7e0c-4162-b79d-2f0690b543da', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 100, 100, now());
-    VALUES ('9e41792b-193b-48e0-98e1-478875020586', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 100, 100, now());
-    VALUES ('b4dbe5b9-2091-40ce-8862-ddc4b1785179', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 100, 100, now());
+INSERT INTO public.auto_embed_images(id, embed_id, url, proxy_url, height, width, created_at)
+	VALUES ('fb64b906-7e0c-4162-b79d-2f0690b543da', 'd95d4370-2629-4d85-85ea-b8d623dbaff4', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 'https://cafefcdn.com/thumb_w/640/203337114487263232/2023/6/15/avatar1686801739290-16868017399091383965775.jpg', 100, 100, now());
 
-INSERT INTO public.auto_embed_footer(id, text, icon_url, url, created_at)
-	VALUES ('e0a1870a-8cfc-407d-9492-e8a45c0d613d', 'Hello footer', 'icon', 'url', now());
-    VALUES ('88003cd5-724d-48bc-859a-197e6c0a3b59', 'Hello footer', 'icon', 'url', now());
+INSERT INTO public.auto_embed_footers(id, embed_id, text, icon_url, url, created_at)
+	VALUES ('e0a1870a-8cfc-407d-9492-e8a45c0d613d', 'd95d4370-2629-4d85-85ea-b8d623dbaff4', 'Hello footer 1', 'icon', 'https://www.investopedia.com/terms/d/deltahedging.asp', now());
 
-INSERT INTO public.auto_embed_video(id, url, height, width, created_at)
-	VALUES (uuid_generate_v4(), 'video url 1', 110, 50, now());
+INSERT INTO public.auto_embed_videos(id, embed_id, url, height, width, created_at)
+	VALUES (uuid_generate_v4(), 'd95d4370-2629-4d85-85ea-b8d623dbaff4', 'https://arxiv.org/pdf/2210.10760.pdf', 110, 50, now());
