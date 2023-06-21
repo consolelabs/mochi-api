033f06fe4d72c2deecf4f9060ef666dc80e91953
Huy Nguyen Quang <39359294+quanghuynguyen1902@users.noreply.github.com>
2023-06-21 21:42:22 +0700

    chore: refactor emoji api

--- pkg/entities/emojis.go
+++ pkg/entities/emojis.go
@@ -1,13 +1,17 @@
 package entities
 
 import (
+	"fmt"
+	"regexp"
+
 	"gorm.io/gorm"
 
 	"github.com/defipod/mochi/pkg/model"
 	"github.com/defipod/mochi/pkg/model/errors"
 )
 
-func (e *Entity) GetListEmojis(codes []string) ([]*model.ProductMetadataEmojis, error) {
+func (e *Entity) GetListEmojis(codes []string) ([]*model.EmojiData, error) {
+	// 1. get list emojis from db
 	emojis, err := e.repo.Emojis.ListEmojis(codes)
 	if err != nil {
 		e.log.Error(err, "[entity.GetListEmojis] repo.emojis.GetListEmojis failed")
@@ -18,5 +22,32 @@ func (e *Entity) GetListEmojis(codes []string) ([]*model.ProductMetadataEmojis, 
 		return nil, err
 	}
 
-	return emojis, nil
+	// 2. convert to emojiData struct
+	emojiDatas := make([]*model.EmojiData, 0)
+	for _, emoji := range emojis {
+		if emoji == nil {
+			continue
+		}
+		if emoji.DiscordId == nil {
+			continue
+		}
+
+		var emojiUrl string
+
+		// 2.1 get id of emoji -> regex number which has length >= 19
+		re := regexp.MustCompile("[0-9]{19,}")
+		matchList := re.FindAllString(*emoji.DiscordId, -1)
+		if len(matchList) > 0 {
+			id := matchList[0]
+			emojiUrl = fmt.Sprintf("https://cdn.discordapp.com/emojis/%s.png?size=240&quality=lossless", id)
+		}
+
+		emojiDatas = append(emojiDatas, &model.EmojiData{
+			Code:     emoji.Code,
+			Emoji:    *emoji.DiscordId,
+			EmojiUrl: emojiUrl,
+		})
+	}
+
+	return emojiDatas, nil
 }
