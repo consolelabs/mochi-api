2b69d06ee960166ab1f5b2230f50acec910eedd7
huynguyenh <hoanghuy2908@gmail.com>
2023-06-23 15:21:09 +0700

    chore: migration seed

--- migrations/seed/auto_trigger.sql
+++ migrations/seed/auto_trigger.sql
@@ -16,18 +16,19 @@ INSERT INTO public.auto_triggers(id, discord_guild_id, user_discord_id, name, st
 INSERT INTO public.auto_triggers(id, discord_guild_id, user_discord_id, name, status, updated_at, created_at) VALUES (2, '711823851117608990', '711823390000021556', 'React heart in general', true, now(), now());
 
 -- Seed data
-INSERT INTO public.auto_actions(id, trigger_id, type_id, channel_ids, name, content, action_data, created_at, index) VALUES (1, 1, 2, '', 'Do action 1', 'Hello world', '', now(), 1);
-INSERT INTO public.auto_actions(id, trigger_id, type_id, channel_ids, name, content, action_data, created_at, index) VALUES (2, 2, 2, '', 'Do action 2', 'Hello world', '', now(), 2);
-INSERT INTO public.auto_actions(id,  trigger_id, type_id, channel_ids, name, content, action_data, created_at, index) VALUES (3,2, 8, '', 'Do action 3', 'Hello world 3', '', now(), 3);
+INSERT INTO "public"."auto_actions" ("id", "user_ids", "trigger_id", "type_id", "channel_ids", "index", "action_data", "name", "content", "then_action_id", "limit_per_user", "created_at") VALUES
+(1, NULL, 1, 2, '', 1, '', 'Do action 1', 'Hello world', NULL, 1, '2023-06-23 05:57:07.797197'),
+(2, NULL, 2, 2, '', 2, '', 'Do action 2', 'Hello world', NULL, 1, '2023-06-23 05:57:09.539787'),
+(3, NULL, 2, 8, '', 3, '', 'Do action 3', 'Hello world 3', NULL, 1, '2023-06-23 05:57:10.905286');
+
 
 -- Seed data condition
-INSERT INTO public.auto_conditions(id, trigger_id, type_id, channel_id, index, platform, updated_at, created_at) VALUES (1, 2, 1, '711823851117608992', 1, 'discord', now(), now());
+INSERT INTO public.auto_conditions(id, trigger_id, type_id, channel_id, index, platform, updated_at, created_at) VALUES (1, 2, 3, '1072722777687199744', 1, 'discord', now(), now());
 
 -- Seed data condition values
-INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches) VALUES (1, 1, 7, 1, '', now(), '');
-INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches) VALUES (2, 1, 7, 2, 'in', now(), '');
-INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches) VALUES (3, 1, 7, 1, 'in', now(), '1115906135799648257,711823851117608990');
-INSERT INTO public.auto_condition_values(id, condition_id, type_id, index, operator, created_at, matches) VALUES (4, 1, 9, 2, '==', now(), '<:pepeheart:867454854686048256>');
+INSERT INTO "public"."auto_condition_values" ("id", "condition_id", "child_id", "type_id", "index", "operator", "matches", "created_at") VALUES
+(3, 1, NULL, 7, 1, 'in', '1115906135799648257,711823851117608990,820988147399393322,462663954813157376', '2023-06-23 06:00:13.571917'),
+(4, 1, NULL, 3, 2, '==', '<:pepeheart:867454854686048256>', '2023-06-23 06:00:33.452028');
 
 INSERT INTO public.auto_embeds(id, author_id, action_id, title, description, color, url, type, fields, created_at)
 	VALUES (1, 'fbaca19d-4ecc-4627-92b6-81858536f921', 2, 'Embed 1', 'descript 1', '#000000', 'https://openai.com/research/measuring-goodharts-law', 'some types', 'any fields', now());
@@ -43,7 +44,7 @@ INSERT INTO public.auto_embed_videos(id, embed_id, url, height, width, created_a
 
 
 -- Seed data to action transfer vault
-INSERT INTO public.auto_actions(id,  trigger_id, type_id, channel_ids, name, content, action_data, created_at) VALUES (1,2, 10, '', 'Transfer vault 1', 'Transfer vault by address', '{
+INSERT INTO public.auto_actions(id,  trigger_id, type_id, channel_ids, name, content, action_data, created_at) VALUES (4, 2, 10, '', 'Transfer vault 1', 'Transfer vault by address', '{
     "discord_guild_id": "711823851117608990",
     "vault_id": 2,
     "message": "Send money to treasurer",
