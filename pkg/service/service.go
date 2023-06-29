a9e410519604c2725b8d849301c3a3551dada352
trkhoi <ngotrongkhoi112@gmail.com>
2023-06-29 16:39:18 +0700

    feat: cache sui

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -111,7 +111,7 @@ func NewService(
 		MochiProfile:  mochiprofile.NewService(&cfg, log),
 		MochiPay:      mochipay.NewService(&cfg, log),
 		ChainExplorer: chainexplorer.NewService(cfg, log),
-		Sui:           sui.New(&cfg, log),
+		Sui:           sui.New(&cfg, log, cache),
 		Birdeye:       birdeye.NewService(&cfg, log, cache),
 		Swap:          swap.New(&cfg, log),
 		Skymavis:      skymavis.New(&cfg, cache),
