1b0d040aa7aa6f2afe195a48e7127b17e65de019
Quang Le <quanglm.ops@gmail.com>
2023-06-30 10:56:02 +0700

    refactor: reduce query time by selecting specific cols (#992)

--- pkg/repo/wallet_snapshot/pg.go
+++ pkg/repo/wallet_snapshot/pg.go
@@ -21,9 +21,9 @@ func (pg *pg) Create(walletSnapshot *model.WalletSnapshot) (*model.WalletSnapsho
 }
 
 func (pg *pg) GetSnapshotInTime(address string, time time.Time) (snapshots []model.WalletSnapshot, err error) {
-	return snapshots, pg.db.Table("wallet_snapshot").Where("wallet_address = ? and snapshot_time >= ?", address, time).Order("snapshot_time asc").Find(&snapshots).Error
+	return snapshots, pg.db.Table("wallet_snapshot").Select("total_usd_balance").Where("wallet_address = ? and snapshot_time >= ?", address, time).Order("snapshot_time asc").Find(&snapshots).Error
 }
 
 func (pg *pg) GetLatestInPast(address string, time time.Time) (snapshots []model.WalletSnapshot, err error) {
-	return snapshots, pg.db.Table("wallet_snapshot").Where("wallet_address = ? and snapshot_time < ?", address, time).Order("snapshot_time desc").Limit(20).Find(&snapshots).Error
+	return snapshots, pg.db.Table("wallet_snapshot").Select("total_usd_balance").Where("wallet_address = ? and snapshot_time < ?", address, time).Order("snapshot_time desc").Limit(20).Find(&snapshots).Error
 }
