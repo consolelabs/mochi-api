c98a846c0b131d7f0cc4a5849f04c0315a92e84e
trkhoi <ngotrongkhoi112@gmail.com>
2023-07-06 17:48:13 +0700

    fix: route jupyter

--- pkg/entities/swap_util.go
+++ pkg/entities/swap_util.go
@@ -259,7 +259,7 @@ func (e *Entity) EnrichTokenMochiPay(route *response.SwapRouteResponse) error {
 		}
 	}
 
-	if route.Provider == "solana" {
+	if route.Provider == "jupyter" {
 		quoteResp := &response.JupyterQuoteResponse{}
 		quoteByte, _ := json.Marshal(route.Data.RouteSummary)
 		err := json.Unmarshal(quoteByte, quoteResp)
