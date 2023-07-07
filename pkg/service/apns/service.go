e9d1ba01e7c548cac93162a6c079333cfa7391dc
huynguyenh <hoanghuy2908@gmail.com>
2023-07-07 16:15:06 +0700

    chore: remove apns

--- pkg/service/apns/service.go
+++
@@ -1,7 +0,0 @@
-package apns
-
-import "github.com/sideshow/apns2"
-
-type Service interface {
-	PushNotificationToIos(pushToken string, price float64, trend string, token string) (*apns2.Response, error)
-}
