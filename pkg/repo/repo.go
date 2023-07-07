21f462436134bc108a9b7267f65b91053bef7783
trkhoi <ngotrongkhoi112@gmail.com>
2023-07-07 13:19:37 +0700

    chore: clean up telegram code

--- pkg/repo/repo.go
+++ pkg/repo/repo.go
@@ -108,8 +108,6 @@ import (
 	usernftwatchlistitem "github.com/defipod/mochi/pkg/repo/user_nft_watchlist_items"
 	usersubmittedad "github.com/defipod/mochi/pkg/repo/user_submitted_ad"
 	usertag "github.com/defipod/mochi/pkg/repo/user_tag"
-	usertelegram "github.com/defipod/mochi/pkg/repo/user_telegram"
-	usertelegramdiscordassociation "github.com/defipod/mochi/pkg/repo/user_telegram_discord_association"
 	usertokenpricealert "github.com/defipod/mochi/pkg/repo/user_token_price_alert"
 	usertokensupportrequest "github.com/defipod/mochi/pkg/repo/user_token_support_request"
 	userwallet "github.com/defipod/mochi/pkg/repo/user_wallet"
@@ -181,7 +179,6 @@ type Repo struct {
 	UserWatchlistItem                    userwatchlistitem.Store
 	GuildConfigGroupNFTRole              guildconfiggroupnftrole.Store
 	CoingeckoSupportedTokens             coingeckosupportedtokens.Store
-	UserTelegramDiscordAssociation       usertelegramdiscordassociation.Store
 	MessageReaction                      messagereaction.Store
 	UserNftWatchlistItem                 usernftwatchlistitem.Store
 	ConversationRepostHistories          conversationreposthistories.Store
@@ -227,7 +224,6 @@ type Repo struct {
 	UserTokenPriceAlert                  usertokenpricealert.Store
 	CommonwealthDiscussionSubscription   commonwealthdiscussionsubscription.Store
 	UserTokenSupportRequest              usertokensupportrequest.Store
-	UserTelegram                         usertelegram.Store
 	Vault                                vault.Store
 	VaultInfo                            vaultinfo.Store
 	VaultConfig                          vaultconfig.Store
--- pkg/repo/user_telegram/store.go
+++
@@ -1,7 +0,0 @@
-package usertelegram
-
-import "github.com/defipod/mochi/pkg/model"
-
-type Store interface {
-	GetByUsername(username string) (*model.UserTelegram, error)
-}
