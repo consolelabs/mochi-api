9fa166acedb11a8641412d63c232fd3c76bc570f
Huy Nguyen Quang <39359294+quanghuynguyen1902@users.noreply.github.com>
2023-07-14 10:29:31 +0700

    fix: update nft role job

--- pkg/repo/user_nft_balance/pg.go
+++ pkg/repo/user_nft_balance/pg.go
@@ -40,48 +40,51 @@ func (pg *pg) Upsert(balance model.UserNFTBalance) error {
 func (pg *pg) GetUserNFTBalancesByUserInGuild(guildID string) ([]model.UserAddressNFTBalancesByGuild, error) {
 	var res []model.UserAddressNFTBalancesByGuild
 	rows, err := pg.db.Raw(`
-	SELECT DISTINCT ON (temp.user_address)
-		user_address, total_balance, staking_neko
-	FROM (
-		SELECT
-			final_balance.user_address AS user_address,
-			sum(final_balance.balance) AS total_balance,
-			final_config.id AS group_id,
-			final_config.role_id AS role_id,
-			final_config.number_of_tokens AS number_of_tokens,
-			final_balance.staking_nekos AS staking_neko
-		FROM (
-			SELECT
-				bals.user_address,
-				bals.nft_collection_id,
-				bals.balance,
-				bals.staking_nekos
-			FROM
-				user_nft_balances AS bals) AS final_balance
-			INNER JOIN (
+		SELECT DISTINCT ON (temp.user_address, nft_collection_id)
+				user_address, total_balance, staking_neko, nft_collection_id
+			FROM (
 				SELECT
-					a.id, config.nft_collection_id, a.guild_id, a.role_id, a.number_of_tokens
-				FROM
-					guild_config_nft_roles AS config
-					INNER JOIN guild_config_group_nft_roles AS a ON config.group_id = a.id
-				WHERE
-					guild_id = ?) AS final_config ON final_config.nft_collection_id = final_balance.nft_collection_id
-			GROUP BY
-				user_address,
-				group_id,
-				role_id,
-				number_of_tokens,
-				staking_nekos) AS temp
-	ORDER BY
-		temp.user_address,
-		temp.number_of_tokens DESC;
+					final_balance.user_address AS user_address,
+					sum(final_balance.balance) AS total_balance,
+					final_config.id AS group_id,
+					final_config.nft_collection_id AS nft_collection_id,
+					final_config.role_id AS role_id,
+					final_config.number_of_tokens AS number_of_tokens,
+					final_balance.staking_nekos AS staking_neko
+				FROM (
+					SELECT
+						bals.user_address,
+						bals.nft_collection_id,
+						bals.balance,
+						bals.staking_nekos
+					FROM
+						user_nft_balances AS bals) AS final_balance
+					INNER JOIN (
+						SELECT
+							a.id, config.nft_collection_id, a.guild_id, a.role_id, a.number_of_tokens
+						FROM
+							guild_config_nft_roles AS config
+							INNER JOIN guild_config_group_nft_roles AS a ON config.group_id = a.id
+						WHERE
+							guild_id = ?) AS final_config ON final_config.nft_collection_id = final_balance.nft_collection_id
+					GROUP BY
+						user_address,
+						group_id,
+						final_config.nft_collection_id,
+						role_id,
+						number_of_tokens,
+						staking_nekos) AS temp
+			ORDER BY
+				temp.user_address,
+				temp.nft_collection_id,
+				temp.number_of_tokens DESC
 	`, guildID).Rows()
 	if err != nil {
 		return nil, err
 	}
 	for rows.Next() {
 		tmp := model.UserAddressNFTBalancesByGuild{}
-		if err := rows.Scan(&tmp.UserAddress, &tmp.TotalBalance, &tmp.StakingNeko); err != nil {
+		if err := rows.Scan(&tmp.UserAddress, &tmp.TotalBalance, &tmp.StakingNeko, &tmp.NftCollectionID); err != nil {
 			return nil, err
 		}
 		res = append(res, tmp)
