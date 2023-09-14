927e615ad7df1fb3275ca345869402c7b099d1b0
Ngo Trong Khoi <39881166+trkhoi@users.noreply.github.com>
2023-09-14 10:29:59 +0700

    chore: add telegram title hashtag (#1179)

--- pkg/model/product_hashtag.go
+++ pkg/model/product_hashtag.go
@@ -3,13 +3,14 @@ package model
 import "time"
 
 type ProductHashtag struct {
-	Id          int64     `json:"id"`
-	Name        string    `json:"name"`
-	Slug        string    `json:"slug"`
-	Description string    `json:"description"`
-	Title       string    `json:"title"`
-	Image       string    `json:"image"`
-	Color       string    `json:"color"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	Id            int64     `json:"id"`
+	Name          string    `json:"name"`
+	Slug          string    `json:"slug"`
+	Description   string    `json:"description"`
+	Title         string    `json:"title"`
+	Image         string    `json:"image"`
+	Color         string    `json:"color"`
+	TelegramTitle string    `json:"telegram_title"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
 }
