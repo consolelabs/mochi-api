03d955cfb24a2e4e2e6b7148f40d56d2e035e64f
Bien <vhbien000@gmail.com>
2023-09-18 09:05:26 +0700

    feat: add tracking key price change percentage

--- pkg/response/friend_tech.go
+++ pkg/response/friend_tech.go
@@ -13,16 +13,17 @@ type FriendTechKeysResponse struct {
 }
 
 type FriendTechKey struct {
-	ID              int64     `json:"id"`
-	CreatedAt       time.Time `json:"createdAt"`
-	UpdatedAt       time.Time `json:"updatedAt"`
-	Address         string    `json:"address"`
-	TwitterUsername string    `json:"twitterUsername"`
-	TwitterPfpUrl   string    `json:"twitterPfpUrl"`
-	ProfileChecked  bool      `json:"profileChecked"`
-	Price           float64   `json:"price"`
-	Supply          int       `json:"supply"`
-	Holders         int       `json:"holders"`
+	ID                    int64     `json:"id"`
+	CreatedAt             time.Time `json:"createdAt"`
+	UpdatedAt             time.Time `json:"updatedAt"`
+	Address               string    `json:"address"`
+	TwitterUsername       string    `json:"twitterUsername"`
+	TwitterPfpUrl         string    `json:"twitterPfpUrl"`
+	ProfileChecked        bool      `json:"profileChecked"`
+	Price                 float64   `json:"price"`
+	Supply                int       `json:"supply"`
+	Holders               int       `json:"holders"`
+	PriceChangePercentage float64   `json:"priceChangePercentage"`
 }
 
 type GetUserFriendTechKeyWatchlistResponse struct {
