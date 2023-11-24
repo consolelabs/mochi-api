d17f4a3aca85ae06eea71fb8a0b7c83d92b9e310
Ngo Trong Khoi <39881166+trkhoi@users.noreply.github.com>
2023-11-24 13:34:40 +0700

    fix: add theme transaction (#1248)

--- pkg/repo/product_theme/pg.go
+++ pkg/repo/product_theme/pg.go
@@ -17,3 +17,7 @@ func NewPG(db *gorm.DB) Store {
 func (pg *pg) Get() (themes []model.ProductTheme, err error) {
 	return themes, pg.db.Table("product_themes").Find(&themes).Error
 }
+
+func (pg *pg) GetByID(id int64) (theme *model.ProductTheme, err error) {
+	return theme, pg.db.Table("product_themes").Where("id = ?", id).First(&theme).Error
+}
--- pkg/repo/product_theme/store.go
+++ pkg/repo/product_theme/store.go
@@ -4,4 +4,5 @@ import "github.com/defipod/mochi/pkg/model"
 
 type Store interface {
 	Get() ([]model.ProductTheme, error)
+	GetByID(id int64) (theme *model.ProductTheme, err error)
 }
