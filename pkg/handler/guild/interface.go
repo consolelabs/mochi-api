2197e31ac90de166dc96825c7e91f06e97ae90f7
trkhoi <ngotrongkhoi112@gmail.com>
2023-11-28 15:18:35 +0700

    fix: restore api create guild

--- pkg/handler/guild/interface.go
+++ pkg/handler/guild/interface.go
@@ -4,6 +4,7 @@ import "github.com/gin-gonic/gin"
 
 type IHandler interface {
 	GetGuilds(c *gin.Context)
+	CreateGuild(c *gin.Context)
 	GetGuild(c *gin.Context)
 	ListMyGuilds(c *gin.Context)
 	UpdateGuild(c *gin.Context)
