a7adeec51a642a47febc2d54d2ab1384816ab894
Ngo Trong Khoi <39881166+trkhoi@users.noreply.github.com>
2023-12-08 18:58:59 +0700

    fix: enrich template tx (#1274)

--- pkg/repo/product_hashtag/store.go
+++ pkg/repo/product_hashtag/store.go
@@ -4,4 +4,5 @@ import "github.com/defipod/mochi/pkg/model"
 
 type Store interface {
 	GetByAlias(alias string) (p *model.ProductHashtag, err error)
+	GetBySlug(slug string) (p *model.ProductHashtag, err error)
 }
